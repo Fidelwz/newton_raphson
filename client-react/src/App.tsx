6f5cc6c0a63c8f9bb793a4af3b72d2dc46a400e2
Fidelwz <fidelcontreras2002@gmail.com>
2025-11-13 00:54:53 -0600

    Pruebas en heroku en proceso

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -1,26 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:5000/api/users')
+      .then(response => response.json())
+      .then(data => setUsers(data.users));
+  }, []);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h2>Users from API:</h2>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              {user.first_name}
+            </li>
+          ))}
+        </ul>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
--- server-flask/main.py
+++ server-flask/main.py
@@ -9,7 +9,11 @@ def home():
 
 @app.route('/api/users')
 def get_users():
-    return {"users": ["Alice", "Bob", "Charlie"]}
+    return {"users": [
+        {"id": 1, "first_name": "Alice", "last_name": "Smith"},
+        {"id": 2, "first_name": "Bob", "last_name": "Johnson"},
+        {"id": 3, "first_name": "Charlie", "last_name": "Brown"}
+    ]}
 
 if __name__ == '__main__':
     app.run(debug=True) 
